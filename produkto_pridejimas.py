e431a893e16bb8482730a589a4a165c16cb8bfcd
br0nke <mantulisr@gmail.com>
2024-01-03 12:35:23 +0000

    update musu

---
+++ produkto_pridejimas.py
@@ -0,0 +1,6 @@
+#prideti produkta i saldytuva
+def prideti_produktą(saldytuvas, produktas, kiekis):
+    if produktas in saldytuvas:
+        saldytuvas[produktas] += kiekis
+    else:
+        saldytuvas[produktas] = kiekis
