873f4af820762bd694da29181e63321dcbe1663f
MartynasPupkus <pupkus.martynas@gmail.com>
2024-01-12 09:28:50 +0200

    Istrinti nereikalingi failai

--- produkto_pridejimas.py
+++
@@ -1,8 +0,0 @@
-#prideti produkta i saldytuva
-def prideti_produktą(saldytuvas, produktas, kiekis):
-    if produktas in saldytuvas:
-        saldytuvas[produktas] += kiekis
-    else:
-        saldytuvas[produktas] = kiekis
-    #PAKEITIMAS
-    
